dba9c8a63c3ae2e670a80b29178bc7b5cf296b02
Cody Fincher <cody.fincher@accenture.com>
2019-11-25 11:34:56 -0600

    updated exception block

--- files/grantAsmPrivileges.sql
+++ files/grantAsmPrivileges.sql
@@ -1,6 +1,6 @@
 SET ECHO OFF
 
-SET SERVEROUTPUT OFF
+SET SERVEROUTPUT ON
 
 SET VERIFY OFF
 
@@ -12,18 +12,27 @@ SET HEADING OFF
 
 DECLARE
     monuser         VARCHAR2(25);
-    user_exist      INTEGER;
     profile_exist   INTEGER;
+    user_exists exception;
     table_not_found EXCEPTION;
-    PRAGMA exception_init ( table_not_found, -00942 );
+    PRAGMA exception_init ( user_exists, -01920 );
 BEGIN
     monuser := '&1';
-    dbms_output.put_line('creating user ' || monuser);
-    EXECUTE IMMEDIATE 'create user '
-                      || monuser
-                      || ' identified by "&2"';
+    begin
+        dbms_output.put_line('creating user ' || monuser);
+        EXECUTE IMMEDIATE 'create user '
+                        || monuser
+                        || ' identified by "&2"';
+    exception when user_exists then
+        dbms_output.put_line('user already exists.  altering ' || monuser);
+
+        EXECUTE IMMEDIATE 'alter user '
+                  || monuser
+                  || ' identified by "&2"';
+
+    end;
     EXECUTE IMMEDIATE 'grant sysasm to ' || monuser;
-    dbms_output.put_line('OMC ASM User Created');
+    dbms_output.put_line('OMC ASM User Deployed');
 END;
 /
 
